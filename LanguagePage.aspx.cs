688b032c2708400296462d93a574da8abaaf7ca7
zhu-wenxiang <zwx199288@163.com>
2024-11-23 10:44:20 +0000

    Initial commit

---
+++ LanguagePage.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CookieTest
+{
+    public partial class LanguagePage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            String text2 = DateTime.Now.ToString("yyyyMMdd");
+            Label1.Text = text2;
+        }
+    }
+}
